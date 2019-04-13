ecc7484bfefc0bd5d1d8c0b65a922232382a5ba2
vikkid507 <ubuntu@ip-172-31-4-84.ec2.internal>
2019-04-13 14:45:54 +0000

    AchiStar Technologies First DevOps

---
+++ achiStar.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Dharmendra Gour
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is AchiStat Technologies");
+System.out.println("DevOp Implementation");
+
+	}
+
+}
+
